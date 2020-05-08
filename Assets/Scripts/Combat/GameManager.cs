a9fc079e4551768801a377688c7a7e3f98edacbc
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2020-05-08 17:26:47 -0500

    Added Text

--- Assets/Scripts/Combat/GameManager.cs
+++ Assets/Scripts/Combat/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour
             enemiesKilled = 0;
         }
 
-        if (totalEnemiesKilled >= 18) {
+        if (totalEnemiesKilled >= 13) {
             SceneManager.LoadScene("WinScene");
         }
 
